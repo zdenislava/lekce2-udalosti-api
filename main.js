adaeee0e22b7ac47dcb0bb3d25b5ae3bb9e52fb8
zdenislava <navarikova@seznam.cz>
2021-03-16 20:40:16 +0100

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 // tady je místo pro náš program
+
+/*let nadpis = document.querySelector("h1");
+nadpis.classList.toggle("zeleny");*/
+
+let paragraf = document.querySelector(".odstavec");
+
+//onmouseover a onmouseout použít
+
+function boldText() {
+    paragraf.style.fontWeight="bold";
+}
+
+function normalText(){
+    paragraf.style.fontWeight="normal";
+}
+
+function colorChange(){
+    paragraf.classList.toggle("cervena");
+}
+
+//zvětší seo 1 pixel vždy, ne jen na 17px
+
+function biggerFont(){
+    paragraf.style.fontSize="17px";
+}
